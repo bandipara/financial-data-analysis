0021f40a78f1a57c080fd1e4477da6da8dafe905
Jacob Menchak <55728549+jmmencha@users.noreply.github.com>
2020-03-30 17:29:11 -0400

    updated code and comments

--- JM/api_keys.py
+++ JM/api_keys.py
@@ -1,2 +1,2 @@
-# World Trading Data API Key
-my_api_token = "YOUR_World_Trading_Data_API_Key_HERE"
+# World Trading Data API Key:
+world_trading_data_api_token = "YOUR_World_Trading_Data_API_Key_HERE"
