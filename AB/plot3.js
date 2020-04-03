5c107b78e28d099390d2773017874eff8ca278b6
bandipara <b.andipara@hotmail.com>
2020-04-02 22:50:59 -0400

    Added Chartist

---
+++ AB/plot3.js
@@ -0,0 +1,52 @@
+
+function makeplot() {
+  Plotly.d3.csv("allstocks.csv", function(data){ 
+      processData(data) } );
+
+};
+ 
+function processData(allRows) {
+
+ console.log(allRows);
+ var x = [], y = [];
+
+ for (var i=0; i<allRows.length; i++) {
+   row = allRows[i];
+   x.push( row['symbol'] );
+   y.push( row['price'] );
+ }
+ console.log( 'X',x, 'Y',y,);
+ makePlotly( x, y);
+}
+
+function makePlotly( x, y){
+ var plotDiv = document.getElementById('.ct-chart');
+ var traces = [{
+   x: x, 
+   y: y,
+   type: "bar"
+ }];
+
+ new Chartist.Line('.ct-chart', traces, 
+   {title: 'Stock Prices of All Banks (USD)'});
+};
+ makeplot();
+
+// var data = {
+//   labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+//   series: [
+//     [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+//   ]
+// };
+
+// var options = {
+//   high: 10,
+//   low: -10,
+//   axisX: {
+//     labelInterpolationFnc: function(value, index) {
+//       return index % 2 === 0 ? value : null;
+//     }
+//   }
+// };
+
+// new Chartist.Line('.ct-chart', data, options);
--- AB/plot5.js
+++ AB/plot5.js
@@ -1,40 +1,40 @@
 
 
 
-//***********************chart5***************************/
+// //***********************chart5***************************/
 
-//Variable to change the title
-var stock_hist= "BAC"
-//Plot the chart
-function makeplot5() {
-    Plotly.d3.csv("WTD_API_AIG_HistoricalMarketData.csv", function(data){ 
-        processData5(data) } );
+// //Variable to change the title
+// var stock_hist= "BAC"
+// //Plot the chart
+// function makeplot5() {
+//     Plotly.d3.csv("WTD_API_AIG_HistoricalMarketData.csv", function(data){ 
+//         processData5(data) } );
   
-  };
+//   };
    
-  function processData5(allRows) {
+//   function processData5(allRows) {
   
-   console.log(allRows);
-   var x = [], y = [];
+//    console.log(allRows);
+//    var x = [], y = [];
   
-   for (var i=0; i<allRows.length; i++) {
-     row = allRows[i];
-     x.push( row['Date'] );
-     y.push( row['Open'] );
-   }
-   console.log( 'X',x, 'Y',y,);
-   makePlotly5( x, y);
-  }
+//    for (var i=0; i<allRows.length; i++) {
+//      row = allRows[i];
+//      x.push( row['Date'] );
+//      y.push( row['Open'] );
+//    }
+//    console.log( 'X',x, 'Y',y,);
+//    makePlotly5( x, y);
+//   }
   
-  function makePlotly5( x, y){
-   var plotDiv = document.getElementById("plot5");
-   var traces = [{
-     x: x, 
-     y: y,
-     type: "bar"
-   }];
+//   function makePlotly5( x, y){
+//    var plotDiv = document.getElementById("plot5");
+//    var traces = [{
+//      x: x, 
+//      y: y,
+//      type: "bar"
+//    }];
   
-   Plotly.newPlot('plot5', traces, 
-     {title: `Historical Data ${stock_hist}`});
-  };
-   makeplot5();
+//    Plotly.newPlot('plot5', traces, 
+//      {title: `Historical Data ${stock_hist}`});
+//   };
+//    makeplot5();
