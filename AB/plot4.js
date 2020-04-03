a214a72fdfc3a4cf7874161fb9e92ace6e37bbca
bandipara <b.andipara@hotmail.com>
2020-04-03 01:01:17 -0400

    Added Search and Populating Table based on Search

--- AB/plot4.js
+++ AB/plot4.js
@@ -1,47 +1,53 @@
 
-// // ****************Read Data from API*******************
+// ****************Read Data from API*******************
 
-// // const url = new URL(
-// //     "https://intraday.worldtradingdata.com/api/v1/intraday"
-// // );
+// const url = new URL(
+//     "https://intraday.worldtradingdata.com/api/v1/intraday"
+// );
 
-// // let params = {
-// //     "symbol": "JPM",
-// //     "api_token": "I6g4IFLVfJ7bWyxF94Jm2ufCQzDYTNwIRHIfutPRr3qiir60aq1LIG5qabsX",
-// //     "interval": "1",
-// //     "range": "1",
-// // };
-// // Object.keys(params)
-// //     .forEach(key => url.searchParams.append(key, params[key]));
+// let params = {
+//     "symbol": "JPM",
+//     "api_token": "I6g4IFLVfJ7bWyxF94Jm2ufCQzDYTNwIRHIfutPRr3qiir60aq1LIG5qabsX",
+//     "interval": "1",
+//     "range": "1",
+// };
+// Object.keys(params)
+//     .forEach(key => url.searchParams.append(key, params[key]));
 
-// // fetch(url, {
-// //     method: "GET",
-// // })
-// //     .then(response => response.json())
-// //     //passing json data to function chartdata
-// //     .then(json => chartdata(json));
-
-
-
-// // //*******************Test Code************************
-// // var trace1 = {
-// //   x: ["beer", "wine", "martini", "margarita",
-// //     "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-// //   y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-// //   type: "bar"
-// // };
-
-// // var data = [trace1];
-
-// // var layout = {
-// //   title: "'Bar' Chart"
-// // };
-
-// // Plotly.newPlot("plot1", data, layout);
+// fetch(url, {
+//     method: "GET",
+// })
+//     .then(response => response.json())
+//     //passing json data to function chartdata
+//     .then(json => chartdata(json));
 
 
 
-// // //*******************CHART-4************************
+// //*******************Test Code************************
+// var trace1 = {
+//   x: ["beer", "wine", "martini", "margarita",
+//     "ice tea", "rum & coke", "mai tai", "gin & tonic"],
+//   y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+//   type: "bar"
+// };
+
+// var data = [trace1];
+
+// var layout = {
+//   title: "'Bar' Chart"
+// };
+
+// Plotly.newPlot("plot1", data, layout);
+
+
+
+// //*******************CHART-4************************
+// const searchInput =document.getElementById('stockInput');
+
+// searchInput.addEventListener('input', () => {
+//     console.log('input event fired');
+//     //d3.event.preventDefault();
+//     var stock = d3.select("#stockInput").node().value;
 
 // function makeplot() {
 //   Plotly.d3.csv("allstocks.csv", function(data){ 
@@ -77,6 +83,6 @@
 //  makeplot();
 
 
-
+// });
 
  
